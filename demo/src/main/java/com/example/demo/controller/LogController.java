2eb12ff54a14762d266ed255660b1e76e904bd4a
zhangjiawen <819126279@qq.com>
2019-03-27 16:22:15 +0800

    整合logback

---
+++ demo/src/main/java/com/example/demo/controller/LogController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.util.JsonData;
+
+
+@RestController
+@RequestMapping("/log")
+public class LogController {
+
+	
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
+	@GetMapping("log")
+	public Object testLog(){
+		
+		logger.debug("this is debug level");
+		logger.info("this is info level ");
+		logger.warn("this is warn level ");
+		logger.error("this is error level");
+		return JsonData.buildSuccess();
+	}
+	
+}
