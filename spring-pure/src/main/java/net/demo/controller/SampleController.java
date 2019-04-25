fdb6800cdb895676ece8f55503cda16a27e66e62
zhangjiawen <819126279@qq.com>
2019-04-25 17:41:13 +0800

    SpringBoot2.x服务端主动推送SSE

---
+++ spring-pure/src/main/java/net/demo/controller/SampleController.java
@@ -0,0 +1,41 @@
+package net.demo.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.demo.utill.User;
+
+@RestController
+public class SampleController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    @RequestMapping("/canshu")
+    String home2(String id) {
+        return "Hello World!"+id;
+    }
+    @RequestMapping("/test")
+    public Map<String,String> testMap(){
+    	Map<String,String> map = new HashMap<>();
+    	map.put("name", "xdclass");
+    	return map;
+    }
+    
+    
+    @GetMapping("/testjson")
+    public Object testjson(){
+    	
+    	return new User(111, "abc123", "10001000", new Date());
+    }
+    
+    
+    
+    
+}
